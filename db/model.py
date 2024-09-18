44488d79370e23c22f05c77803343258eb14d685
lauratulian <lau.tulian01@gmail.com>
2024-09-18 09:23:38 -0300

    Actualizando rama main con cambios

--- db/model.py
+++ db/model.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-
 class Auth_User(Base):
     __tablename__ = "auth_user"
     id = Column(Integer, primary_key=True, autoincrement=True)
