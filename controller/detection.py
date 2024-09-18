f409ca897c305e2f54b1313c40189f5164b5928f
lauratulian <lau.tulian01@gmail.com>
2024-09-18 09:05:01 -0300

    Primera version

---
+++ controller/detection.py
@@ -0,0 +1,79 @@
+from ultralytics import YOLO
+from PIL import Image, ImageTk
+import cv2
+import imutils
+import numpy as np
+
+
+def abrir_camara():
+    model = YOLO(r"C:\Users\lautu\OneDrive\Escritorio\PPE Detection\controller\best.pt")
+
+    cap = cv2.VideoCapture(0)
+
+    if not cap.isOpened():
+        print("Error al abrir la cámara.")
+        return
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error al capturar el fotograma.")
+            break
+
+        frame = imutils.resize(frame, width=640)
+        im = Image.fromarray(frame)
+        img = ImageTk.PhotoImage(image=im)
+
+        # Realizar detección con YOLO
+        results = model(frame)
+        annotated_frame = results[0].plot()
+
+        # Mostrar resultados en tiempo real
+        cv2.imshow("Detección en tiempo real", annotated_frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+model = YOLO(r"C:\Users\lautu\OneDrive\Escritorio\PPE Detection\controller\best.pt")
+
+def detectar_imagen():
+    # Lógica para seleccionar imagen y procesar
+    from tkinter import filedialog
+    archivo = filedialog.askopenfilename(filetypes=[('Imagenes', '*.jpg')])
+    if archivo:
+        image = cv2.imread(archivo)
+        
+        if image is None:
+            print("Error al cargar la imagen.")
+            return
+        results = model(image)
+        annotated_image = results[0].plot()
+        cv2.imshow("Detección en imagen", annotated_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+def detectar_video():
+    # Lógica para seleccionar video y procesar
+    from tkinter import filedialog
+    archivo = filedialog.askopenfilename(filetypes=[('Videos', '*.mp4')])
+    if archivo:
+        cap = cv2.VideoCapture(archivo)
+        if not cap.isOpened():
+            print("Error al abrir el video.")
+            return
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                print("Error al capturar el fotograma o video terminado.")
+                break
+            results = model(frame)
+            annotated_frame = results[0].plot()
+            cv2.imshow("Detección en video", annotated_frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
