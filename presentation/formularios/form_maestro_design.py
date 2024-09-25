161f75f7f8327548d047f71dc794bb69a02deee0
Agostina Chiara <agosschiara25@gmail.com>
2024-09-25 17:12:31 -0300

    version final

--- presentation/formularios/form_maestro_design.py
+++ presentation/formularios/form_maestro_design.py
@@ -8,17 +8,17 @@ import presentation.util.util_ventana as util_ventana
 import presentation.util.util_imagenes as util_img
 from PIL import Image, ImageTk
 import cv2
-import numpy as np
 import fitz  
 
 class FormMaestro(tk.Tk):
 
     def __init__(self):
         super().__init__()
-        self.logo = util_img.leer_imagen("./presentation/imagenes/logo.png", (200, 200))
+        self.logo = util_img.leer_imagen(r"presentation\imagenes\logo.png", (100, 100))
         self.config_window()
         self.model = YOLO('./best.pt')
         self.cap = None
+        self.icon_images = {}
         self.paneles()
         self.controles_barra_superior()        
         self.controles_menu_lateral()
@@ -28,9 +28,8 @@ class FormMaestro(tk.Tk):
 
     
     def config_window(self):
-        # Configuración inicial de la ventana
         self.title('Python UTN')
-        self.iconbitmap("./presentation/imagenes/logo.png")
+        self.iconbitmap(r"presentation\imagenes\logo.ico")
         w, h = 1024, 600        
         util_ventana.centrar_ventana(self, w, h)        
 
@@ -46,13 +45,11 @@ class FormMaestro(tk.Tk):
         self.cuerpo_principal.pack(side=tk.RIGHT, fill='both', expand=True)
     
     def controles_barra_superior(self):
-        # Configuración de la barra superior
-        font_awesome = font.Font(family='FontAwesome', size=12)
-
-         # Botón del menú lateral
-        self.buttonMenuLateral = tk.Button(self.barra_superior, text="\uf0c9", font=font_awesome,
+        # Botón del menú lateral
+        self.icon_images["menu"] = icon_to_image("bars", fill="#FFFFFF", scale_to_width=16)
+        self.buttonMenuLateral = tk.Button(self.barra_superior, image=self.icon_images["menu"],
                                         command=self.toggle_panel, bd=0, bg=COLOR_BARRA_SUPERIOR, fg="white")
-        self.buttonMenuLateral.pack(side=tk.LEFT)
+        self.buttonMenuLateral.pack(side=tk.LEFT, padx=20)
 
         # Etiqueta de título
         self.labelTitulo = tk.Label(self.barra_superior, text="Grupo 8")
@@ -69,53 +66,51 @@ class FormMaestro(tk.Tk):
 
     
     def controles_menu_lateral(self):
-        ancho_menu = 20
-        alto_menu = 2
         font_awesome = font.Font(family='FontAwesome', size=15)
 
+        self.icon_images["live"] = icon_to_image("video", fill="#FFFFFF", scale_to_width=16)
+        self.icon_images["upload"] = icon_to_image("upload", fill="#FFFFFF", scale_to_width=16)
+        self.icon_images["info"] = icon_to_image("info-circle", fill="#FFFFFF", scale_to_width=16)
+        self.icon_images["quit"] = icon_to_image("power-off", fill="#FFFFFF", scale_to_width=16)
+
         # Botones del menú lateral
-        self.buttonLive = tk.Button(self.menu_lateral, command=self.start_camera_detection)
-        self.buttonUpload = tk.Button(self.menu_lateral, command=self.upload_file)
-        self.buttonInfo = tk.Button(self.menu_lateral, command=lambda: self.open_pdf("./presentation/documentos/info.pdf"))
-        self.buttonQuit = tk.Button(self.menu_lateral, command=self.destroy)
+        self.buttonLive = tk.Button(self.menu_lateral, image=self.icon_images["live"], text="Video en vivo", compound=tk.LEFT,
+                                    command=self.start_camera_detection, bd=0, bg=COLOR_MENU_LATERAL, fg="white", font=font_awesome,
+                                    padx=10)
 
-        buttons_info = [
-            ("Video en vivo", "\uf109", self.buttonLive),
-            ("Subir archivo", "\uf007", self.buttonUpload),
-            ("Info", "\uf129", self.buttonInfo),
-        ]
-
-        for text, icon, button in buttons_info:
-            self.configurar_boton_menu(button, text, icon, font_awesome, ancho_menu, alto_menu)
-
-        self.configurar_boton_menu(self.buttonQuit, "Salir", "\uf013", font_awesome, ancho_menu, alto_menu)
-        self.buttonQuit.pack(side=tk.BOTTOM)   
-
+        self.buttonUpload = tk.Button(self.menu_lateral, image=self.icon_images["upload"], text="Subir archivo", compound=tk.LEFT,
+                                      command=self.upload_file, bd=0, bg=COLOR_MENU_LATERAL, fg="white", font=font_awesome,
+                                      padx=10)
         
+        self.buttonInfo = tk.Button(self.menu_lateral, image=self.icon_images["info"], text="Info", compound=tk.LEFT,
+                            bd=0, bg=COLOR_MENU_LATERAL, fg="white", font=font_awesome, padx=10,
+                            command=lambda: self.open_pdf(r"presentation\imagenes\narrativa.pdf"))
+        
+        self.buttonQuit = tk.Button(self.menu_lateral, image=self.icon_images["quit"], text="Salir", compound=tk.LEFT,
+                            command=self.destroy, bd=0, bg=COLOR_MENU_LATERAL, fg="white", font=font_awesome,
+                            padx=10)
+
+        buttons = [self.buttonLive, self.buttonUpload, self.buttonInfo, self.buttonQuit]
+
+        for button in buttons:
+            button.pack(side=tk.TOP, pady=20, anchor="w", fill=tk.X)
+            button.bind("<Enter>", self.on_enter)
+            button.bind("<Leave>", self.on_leave)
+        
+
     def controles_cuerpo(self):
         # Imagen en el cuerpo principal
         label = tk.Label(self.cuerpo_principal, image=self.logo,
                          bg=COLOR_CUERPO_PRINCIPAL)
         label.place(x=0, y=0, relwidth=1, relheight=1)
 
-    def configurar_boton_menu(self, button, text, icon, font_awesome, ancho_menu, alto_menu):
-        button.config(text=f"  {icon}    {text}", anchor="w", font=font_awesome,
-                      bd=0, bg=COLOR_MENU_LATERAL, fg="white", width=ancho_menu, height=alto_menu)
-        button.pack(side=tk.TOP)
-        self.bind_hover_events(button)
-
-    def bind_hover_events(self, button):
-        # Asociar eventos Enter y Leave con la función dinámica
-        button.bind("<Enter>", lambda event: self.on_enter(event, button))
-        button.bind("<Leave>", lambda event: self.on_leave(event, button))
-
-    def on_enter(self, event, button):
+    def on_enter(self, e):
         # Cambiar estilo al pasar el ratón por encima
-        button.config(bg=COLOR_MENU_CURSOR_ENCIMA, fg='white')
+        e.widget['background'] = COLOR_MENU_CURSOR_ENCIMA
 
-    def on_leave(self, event, button):
+    def on_leave(self, e):
         # Restaurar estilo al salir el ratón
-        button.config(bg=COLOR_MENU_LATERAL, fg='white')
+        e.widget['background'] = COLOR_MENU_LATERAL
 
     def toggle_panel(self):
         # Alternar visibilidad del menú lateral
@@ -130,6 +125,31 @@ class FormMaestro(tk.Tk):
         self.content_label.configure(image=image)
         self.content_label.image = image
 
+    def dimensionar_camara(self, frame):
+        self.update_idletasks()
+        window_width = self.cuerpo_principal.winfo_width()
+        window_height = self.cuerpo_principal.winfo_height()
+
+        if isinstance(frame, Image.Image):
+            frame_width, frame_height = frame.size
+        else:
+            frame_height, frame_width = frame.shape[:2]
+
+        scale_width = window_width / frame_width
+        scale_height = window_height / frame_height
+        scale = min(scale_width, scale_height)
+            
+        new_width = int(frame_width * scale)
+        new_height = int(frame_height * scale)
+        
+        if isinstance(frame, Image.Image):
+            resized_frame = frame.resize((new_width, new_height), Image.Resampling.LANCZOS)
+        else:
+            resized_frame = cv2.resize(frame, (new_width, new_height), interpolation=cv2.INTER_AREA)
+            
+        return resized_frame 
+
+   
     def start_camera_detection(self):
         if self.cap and self.cap.isOpened():
             self.cap.release()
@@ -143,9 +163,12 @@ class FormMaestro(tk.Tk):
                 break
 
             frame = cv2.flip(frame, 1)
-            results = self.model(frame)
+
+            results = self.model(self.dimensionar_camara(frame))
             annotated_frame = results[0].plot()
+
             annotated_frame = cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB)
+            
             self.update_display(annotated_frame)
             self.update()
 
@@ -159,10 +182,12 @@ class FormMaestro(tk.Tk):
             self.cap.release()
 
         image = cv2.imread(image_path)
-        resized_image = cv2.resize(image, (640, 480), interpolation=cv2.INTER_AREA)
-        results = self.model.predict(resized_image)
+
+        results = self.model.predict(self.dimensionar_camara(image), imgsz=640, conf=0.6)
         annotated_image = results[0].plot()
+        
         annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+        
         self.update_display(annotated_image)
 
     def process_video(self, video_path):
@@ -176,11 +201,16 @@ class FormMaestro(tk.Tk):
             if not ret:
                 break
 
-            resized_frame = cv2.resize(frame, (640, 480), interpolation=cv2.INTER_AREA)
-            results = self.model.predict(resized_frame)
+            resized_frame = self.dimensionar_camara(frame)
+
+            results = self.model.predict(resized_frame, conf=0.6)
             annotated_frame = results[0].plot()
+
             annotated_frame = cv2.cvtColor(annotated_frame, cv2.COLOR_BGR2RGB)
+            
             self.update_display(annotated_frame)
+            
+            self.update_idletasks()
             self.update()
 
         self.cap.release()
@@ -203,6 +233,9 @@ class FormMaestro(tk.Tk):
 
     def open_pdf(self, pdf_path):
         try:
+            if self.cap and self.cap.isOpened():
+                self.cap.release()
+
             pdf_document = fitz.open(pdf_path)
             
             page = pdf_document.load_page(0)
@@ -210,12 +243,12 @@ class FormMaestro(tk.Tk):
 
             image = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
 
-            image = image.resize((700, 700), Image.Resampling.LANCZOS)
+            resized_image = self.dimensionar_camara(image)
 
-            image_tk = ImageTk.PhotoImage(image)
+            image_tk = ImageTk.PhotoImage(resized_image)
 
             self.content_label.config(image=image_tk)
-            self.content_label.image = image_tk  
+            self.content_label.image = image_tk
 
         except Exception as e:
             messagebox.showerror("Error", f"No se pudo cargar el PDF: {e}")
